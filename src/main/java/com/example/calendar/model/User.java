4f664de368c8f3d38fdde050759d0d0b5c316014
Sergey-Spodarev <spodarev8@gmail.com>
2025-05-02 12:42:11 +0300

    I made fields for the User Database

--- src/main/java/com/example/calendar/model/User.java
+++ src/main/java/com/example/calendar/model/User.java
@@ -1,4 +1,19 @@
 package com.example.calendar.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
 // Сущности БД (классы-таблицы):данные пользователя.
+@Entity//Помечает класс как сущность БД.
+@Table(name = "User") //(опционально) – Указывает имя таблицы, если оно отличается от имени класса.
+@Data //lombok здесь реализуется get,set,tostring,equals,hascod
 public class User {
+    @Id //Обозначает первичный ключ.
+    @Column(name = "userId", unique = false)//unique - говорит что должно быть уникальное значение
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//Указывает стратегию генерации ID (автоинкремент, sequence и т. д.).
+    private Long userId;
+    @Column(name = "userName", nullable = false, unique = true) //Позволяет задать имя столбца, ограничения и другие параметры.
+    private String userName;
+    @Column(name = "password", nullable = false)
+    private String password;
 }
