52fe1d9d0f42d815452eb543cfd2da5a5882243c
Sergey-Spodarev <spodarev8@gmail.com>
2025-05-02 12:52:27 +0300

    Created a UserRepository to work with the Database without writing standard code due to annotations

--- src/main/java/com/example/calendar/repository/UserRepository.java
+++ src/main/java/com/example/calendar/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.example.calendar.repository;
+
+import com.example.calendar.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 //Интерфейсы для работы с БД (наследуются от JpaRepository):запросы к таблице пользователей.
-public class UserRepository {
+@Repository//Помечает интерфейс как Spring-репозиторий (не обязательно, т. к. Spring автоматически распознаёт его через наследование от JpaRepository).
+public interface UserRepository extends JpaRepository<User, Long> {//JpaRepository - автоматически создаст реализацию.
 }
