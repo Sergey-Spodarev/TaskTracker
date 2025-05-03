8238516713cdbbd7aafcf6fd436457d6464ac0b1
Sergey-Spodarev <spodarev8@gmail.com>
2025-05-03 14:50:42 +0300

    added the addition of a user when working BD

--- src/main/java/com/example/calendar/service/UserService.java
+++ src/main/java/com/example/calendar/service/UserService.java
@@ -1,4 +1,36 @@
 package com.example.calendar.service;
+
+import com.example.calendar.DTO.UserDTO;
+import com.example.calendar.model.User;
+import com.example.calendar.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 //Бизнес-логика (например, UserService для регистрации).
+@Service // Помечает класс как сервисный компонент (логика приложения)
+@Transactional //Управляет транзакциями для работы с БД.
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public Boolean checkUserRegistered(String email){
+        return userRepository.findByEmail(email).isPresent();
+    }
+
+    public User registerUser(UserDTO userDTO){
+        if (checkUserRegistered(userDTO.getEmail())){
+            throw new RuntimeException("Пользователь уже существует");//временно так потом надо сообщение пользователю выбивать
+        }
+
+        User user = new User();
+        user.setUserName(userDTO.getUserName());
+        user.setEmail(userDTO.getEmail());
+        user.setPassword(userDTO.getPassword());
+
+        return userRepository.save(user);
+    }
+
 }
