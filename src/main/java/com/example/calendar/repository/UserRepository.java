0dba1a68c9cf85959abb4e1acc5cea9b26298fed
Sergey-Spodarev <spodarev8@gmail.com>
2025-05-03 13:55:12 +0300

    added an email search function

--- src/main/java/com/example/calendar/repository/UserRepository.java
+++ src/main/java/com/example/calendar/repository/UserRepository.java
@@ -4,7 +4,10 @@ import com.example.calendar.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 //Интерфейсы для работы с БД (наследуются от JpaRepository):запросы к таблице пользователей.
 @Repository//Помечает интерфейс как Spring-репозиторий (не обязательно, т. к. Spring автоматически распознаёт его через наследование от JpaRepository).
 public interface UserRepository extends JpaRepository<User, Long> {//JpaRepository - автоматически создаст реализацию.
+    Optional findByEmail(String email);
 }
