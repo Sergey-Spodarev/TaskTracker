be75d45937bda0cb3106c204c68a21b852f31a5d
Sergey-Spodarev <spodarev8@gmail.com>
2025-05-04 15:15:56 +0300

    fixed the method of adding a user

--- src/main/java/com/example/calendar/controller/UserController.java
+++ src/main/java/com/example/calendar/controller/UserController.java
@@ -1,28 +1,33 @@
 package com.example.calendar.controller;
 
+import ch.qos.logback.core.model.Model;
 import com.example.calendar.DTO.UserDTO;
 import com.example.calendar.model.User;
 import com.example.calendar.service.UserService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-//Контроллеры (обработка HTTP-запросов):работа с календарём.
 @RestController
-@RequestMapping("/api/user")//Эта аннотация задает базовый URL для всех методов в классе контроллера
+@RequestMapping("/api/users")  // Лучше использовать множественное число для REST
 public class UserController {
+
     private final UserService userService;
 
-    public UserController(UserService userService){
+    // Внедрение зависимости через конструктор
+    public UserController(UserService userService) {
         this.userService = userService;
     }
 
     @PostMapping("/register")
-    public ResponseEntity<String> register(@RequestBody UserDTO userDTO){
-        User registeredUser = userService.registerUser(userDTO);
-        return ResponseEntity.ok()
-                .body("Пользователь успешно зарегистрирован. ID: " + registeredUser.getUserId());
+    public String registerUser(@RequestParam String username, @RequestParam String email, @RequestParam String password, Model model) {
+        try {
+            UserDTO userDTO = new UserDTO();
+            userDTO.setUserName(username);
+            userDTO.setEmail(email);
+            userDTO.setPassword(password);
+            User registeredUser = userService.registerUser(userDTO);
+            return "redirect:/login?success";
+        } catch (Exception e) {
+            return "register";
+        }
     }
 }
