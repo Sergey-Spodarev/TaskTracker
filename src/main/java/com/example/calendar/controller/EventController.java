9e6063b0cddc6c24670cd6ebc263c98d0d3ea5fe
Sergey-Spodarev <spodarev8@gmail.com>
2025-05-05 03:26:58 +0300

    created the logic for working with http

---
+++ src/main/java/com/example/calendar/controller/EventController.java
@@ -0,0 +1,32 @@
+package com.example.calendar.controller;
+
+import com.example.calendar.DTO.EventDTO;
+import com.example.calendar.model.Event;
+import com.example.calendar.service.EventService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/event")
+public class EventController {
+    private final EventService eventService;
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @PostMapping("/add")
+    public void addEvent(@RequestBody EventDTO eventDTO) {
+        eventService.saveEvent(eventDTO);
+    }
+
+    @GetMapping("/get")
+    public List<EventDTO> getEvents() {
+        return eventService.getCurrentUserEvents();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteEvent(@PathVariable Long id) {
+        eventService.deleteEvent(id);
+    }
+}
