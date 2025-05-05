dcd141cfc735d56ad1612cd8d15bfad9ab0ff66b
Sergey-Spodarev <spodarev8@gmail.com>
2025-05-05 03:29:10 +0300

    changed the variable type from Event to EventDTO

--- src/main/java/com/example/calendar/repository/EventRepository.java
+++ src/main/java/com/example/calendar/repository/EventRepository.java
@@ -1,10 +1,13 @@
 package com.example.calendar.repository;
 
+import com.example.calendar.DTO.EventDTO;
 import com.example.calendar.model.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
-
+    List<EventDTO> findAllByEmail(String email);
 }
