d9afaebf3e06480647949adbd8db7cc6280e48d1
Sergey-Spodarev <spodarev8@gmail.com>
2025-05-05 06:21:27 +0300

    .

--- src/main/java/com/example/calendar/service/EventService.java
+++ src/main/java/com/example/calendar/service/EventService.java
@@ -12,7 +12,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.nio.file.AccessDeniedException;
 import java.util.List;
 import java.util.Optional;
 
