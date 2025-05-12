5c3a342f2ba7b2d86c16b8105254c5ee8db5d3be
Sergey-Spodarev <spodarev8@gmail.com>
2025-05-12 21:31:57 +0300

    created and implemented CustomUserDetailsService

---
+++ src/main/java/com/example/calendar/security/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.example.calendar.security;
+
+import com.example.calendar.model.User;
+import com.example.calendar.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username){
+        User user = userRepository.findByUserName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new CustomUserDetails(user);
+    }
+}
