9da99e9ce23961456276575745d2902c4fad7aca
Sergey-Spodarev <spodarev8@gmail.com>
2025-07-12 02:20:17 +0300

    I added new fields to link them to new tables.

--- src/main/java/com/example/TaskTracker/model/User.java
+++ src/main/java/com/example/TaskTracker/model/User.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Data
 public class User {
@@ -19,4 +22,10 @@ public class User {
     private String password;
 
     private String role;
+
+    @OneToMany(mappedBy = "creator", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Tasks> createTasks = new ArrayList<>();
+
+    @OneToMany(mappedBy = "assignee", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Tasks> assignedTasks = new ArrayList<>();
 }
