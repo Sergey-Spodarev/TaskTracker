335dbad06510cc11e17bcd124b0b52a1a9efe3eb
Sergey-Spodarev <spodarev8@gmail.com>
2025-07-11 19:19:15 +0300

    I fixed all the issues with wrapping the user in UserDetails

---
+++ src/main/java/com/example/TaskTracker/security/CustomUserDetails.java
@@ -0,0 +1,73 @@
+package com.example.TaskTracker.security;
+
+import com.example.TaskTracker.model.User;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Collection;
+
+@Getter
+public class CustomUserDetails implements UserDetails, Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private final String userName;
+    private final String password;
+
+    //эти все поля нужны для наследования от UserDetails
+    private final Collection<? extends GrantedAuthority> authorities;//для работы с полномочиями
+    private final boolean enabled;//Активность аккаунта
+    private final boolean accountNonExpired;//Не истёк ли срок аккаунта
+    private final boolean credentialsNonExpired;//Не истёк ли срок пароля
+    private final boolean accountNonLocked;//Заблокирован ли пользователь
+
+    CustomUserDetails(User user) {
+        this.userName = user.getEmail();
+        this.password = user.getPassword();
+        this.authorities = AuthorityUtils.createAuthorityList(user.getRole());//AuthorityUtils для работы с полномочиями в Spring Security
+
+        this.enabled = true;
+        this.accountNonExpired = true;
+        this.credentialsNonExpired = true;
+        this.accountNonLocked = true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){//Возвращает список прав/ролей пользователя
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return credentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
