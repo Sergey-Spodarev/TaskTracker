d25fd3560a35fd24b2c7331867a1f8220695ee4f
Sergey-Spodarev <spodarev8@gmail.com>
2025-09-02 23:49:21 +0300

    поменял название файла в визуализации

--- src/main/java/com/example/calendar/controller/CalendarController.java
+++ src/main/java/com/example/calendar/controller/CalendarController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class CalendarController {
     @GetMapping("/calendar")
     public String showCalendar() {
-        return "calendar"; // Имя файла без .html
+        return "task"; // Имя файла без .html
     }
 }
